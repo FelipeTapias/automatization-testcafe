f67657f31d8b13b6873836698650c10e046e5895
Felipe Tapias <atapias@sistecredito.com>
2022-01-25 18:22:30 -0500

    Se resuelve el problema del datapicker

--- tests/page_model/registerUser.js
+++ tests/page_model/registerUser.js
@@ -9,6 +9,8 @@ export default class RegisterUser {
     fieldPassword = Selector('#root > div.container.mt-5.w-50 > form > div > div:nth-child(3) > input');
     fieldBirth = Selector('#root > div.container.mt-5.w-50 > form > div > div.w-50.m-auto.mb-5 > div > div > input');
     buttonCreate = Selector('#root > div.container.mt-5.w-50 > form > div > button');
+    birthSelected = Selector('#body > div:nth-child(7) > div > div > div > div > div > div.ant-picker-body > table > tbody > tr:nth-child(3) > td.ant-picker-cell.ant-picker-cell-in-view.ant-picker-cell-selected > div');
+    titlePlace = Selector('#root > div > div.Home > div:nth-child(3) > div:nth-child(1) > h2')
 
     actionRegisterUser = async(t, name, email, password, birth) => {
         await t
@@ -25,8 +27,11 @@ export default class RegisterUser {
             .typeText(this.fieldPassword, password)
             .wait(2000)
             .typeText(this.fieldBirth, birth)
-            .wait(2000)
+            .wait(3000)
+            .click(this.birthSelected)
+            .wait(1000)
             .click(this.buttonCreate)
-            .wait(5000)
+            .wait(3000)
+            .expect(this.titlePlace.innerText).eql('Cerro Pan de Azúcar');
     }
 }
